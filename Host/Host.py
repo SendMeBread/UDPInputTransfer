23f78ff09f3d128c5db7b9cc8b05f6f6b82566b6
SendMeMyBread <joemczero@gmail.com>
2024-11-26 15:43:46 -0500

    Begginings of input transfer, through strings.

--- Host/Host.py
+++ Host/Host.py
@@ -1,4 +1,4 @@
-from threading import Thread
+import time
 import keyboard
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
@@ -8,20 +8,9 @@ class MulticastInputs(DatagramProtocol):
         self.transport.joinGroup("228.0.0.5")
     def datagramReceived(self, datagram, address):
         print("Host received!")
-        if datagram == b"w" or datagram == "w":
-            w_key()
-        if datagram == b"a" or datagram == "a":
-            s_key()
-
-def w_key():
-    if keyboard.is_pressed('w'):
-        keyboard.release('w')
-    else:
-        keyboard.press('w')
-def s_key():
-    if keyboard.is_pressed('w'):
-        keyboard.release('w')
-    else:
-        keyboard.press('w')
+        if b"w" in datagram:
+            keyboard.send("w")
+        if b"a" in datagram:
+            keyboard.send("a")
 reactor.listenMulticast(65535, MulticastInputs(), listenMultiple=True)
 reactor.run()
--- Host/Testing_Client.py
+++ Host/Testing_Client.py
@@ -1,16 +1,15 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
-import time
-
+import keyboard
+keys = ['w', 'a', 's', 'd']
 class MulticastPingClient(DatagramProtocol):
     def startProtocol(self):
         self.transport.joinGroup("228.0.0.5")
-        self.transport.write(b"w", ("228.0.0.5", 65535))
-        time.sleep(4)
-        self.transport.write(b"w", ("228.0.0.5", 65535))
+        while True:
+            key = keyboard.read_key()
+            if key in keys:
+                self.transport.write(bytes(key, encoding='utf-8'), ("228.0.0.5", 65535))
     def datagramReceived(self, datagram, address):
         print(f"Datagram {repr(datagram)} received from {repr(address)}")
-
-
 reactor.listenMulticast(65535, MulticastPingClient(), listenMultiple=True)
 reactor.run()
