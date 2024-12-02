93fc7f533da6b219cff1fa6f7c073374f099332a
SendMeMyBread <joemczero@gmail.com>
2024-12-02 11:57:17 -0500

    We are doing great things today!

--- Client(s)/WASD/Client_D.py
+++ Client(s)/WASD/Client_D.py
@@ -1,14 +1,16 @@
+import sys
+sys.path += ["../Packages/keyboard", "../Packages/twisted"]
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 import keyboard
 class MulticastPingClient(DatagramProtocol):
     def startProtocol(self):
-        self.transport.joinGroup("228.0.0.5")
+        self.transport.joinGroup(sys.argv[1])
         while True:
             key = keyboard.read_key()
             if key == "d":
-                self.transport.write(bytes(key, encoding='utf-8'), ("228.0.0.5", 65535))
+                self.transport.write(bytes(key, encoding='utf-8'), (sys.argv[1], 65535))
     def datagramReceived(self, datagram, address):
         print(f"Datagram {repr(datagram)} received from {repr(address)}")
-reactor.listenMulticast(65535, MulticastPingClient(), listenMultiple=True)
+reactor.listenMulticast(int(sys.argv[2]), MulticastPingClient(), listenMultiple=True)
 reactor.run()
--- Host(s)/LSW:TCS_Host.py
+++ Host(s)/LSW:TCS_Host.py
@@ -1,10 +1,12 @@
+import sys
+sys.path += ["../Packages/keyboard", "../Packages/twisted"]
 import keyboard
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 class MulticastInputs(DatagramProtocol):
     def startProtocol(self):
         self.transport.setTTL(5)
-        self.transport.joinGroup("228.0.0.5")
+        self.transport.joinGroup(sys.argv[1])
     def datagramReceived(self, datagram, address):
         print("Host received!")
         if b"w" in datagram:
@@ -23,5 +25,5 @@ class MulticastInputs(DatagramProtocol):
             keyboard.send("up")
         if b"e" in datagram:
             keyboard.send("down")
-reactor.listenMulticast(65535, MulticastInputs(), listenMultiple=True)
+reactor.listenMulticast(int(sys.argv[2]), MulticastInputs(), listenMultiple=True)
 reactor.run()
